1f32aebeb4be41d17d3e832a06d17dae0882d9ad
Ken Musembi <kenmusembi21@gmail.com>
2019-11-28 15:27:39 +0300

    classes

---
+++ classes.py
@@ -0,0 +1,12 @@
+class Mammals():
+	pass
+	
+class Giraffe(Mammals):
+	pass
+	
+reginald = Giraffe()
+
+def this_is_a_normal_function():
+	print('I am a normal function')
+
+this_is_a_normal_function()
